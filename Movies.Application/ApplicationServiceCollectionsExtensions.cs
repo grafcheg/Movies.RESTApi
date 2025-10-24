423d502b48c7c4c0fba2f8b5494890f30f23381b
Vitaly Efremov <dergraff@gmail.com>
2025-10-24 20:56:17 +1000

    Фикс структуры проекта + добавление шаблона репозитория с временными методами

---
+++ Movies.Application/ApplicationServiceCollectionsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Repositories;
+
+namespace Movies.Application;
+
+public static class ApplicationServiceCollectionsExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddSingleton<IMovieRepository, MovieRepository>();
+
+        return services;
+    }
+}

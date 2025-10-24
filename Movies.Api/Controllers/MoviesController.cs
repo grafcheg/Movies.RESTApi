385c98339c5dce920b85538e03a0989a3a5a5609
Vitaly Efremov <dergraff@gmail.com>
2025-10-24 21:13:11 +1000

    Create Movie endpoint

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Models;
 using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
 
 namespace Movies.Api.Controllers;
 
 [ApiController]
+[Route("api")]
 public class MoviesController : ControllerBase
 {
     private readonly IMovieRepository _movieRepository;
@@ -11,5 +14,21 @@ public class MoviesController : ControllerBase
     public MoviesController(IMovieRepository movieRepository)
     {
         _movieRepository = movieRepository;
+    }
+
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        var movie = new Movie
+        {
+            Id = Guid.NewGuid(),
+            Title = request.Title,
+            YearOfRelease = request.YearOfRelease,
+            Genres = request.Genres.ToList()
+        };
+        
+        await _movieRepository.CreateAsync(movie);
+        
+        return Created($"/api/movies/{movie.Id}", movie);
     }
 }
